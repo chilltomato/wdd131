0aac613e11b9213b3353508f7633c2f9e3ebf155
chilltomato <haz23003@byui.edu>
2024-11-09 13:38:50 -0500

    first!

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+const year = document.querySelector("#year");
+const month = document.querySelector("#month");
+const day = document.querySelector("#day");
+
+const today = new Date();
+
+currentyear.innerHTML = `©️ <span class="highlight">${today.getFullYear()}</span>`;
