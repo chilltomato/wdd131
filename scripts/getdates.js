a23245ed95afc511c44ab58b76841f1915cb2ed0
chilltomato <haz23003@byui.edu>
2024-11-12 13:11:20 -0500

    fixed some crap, have fun now

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,10 @@
 const year = document.querySelector("#year");
 const month = document.querySelector("#month");
 const day = document.querySelector("#day");
-
 const today = new Date();
+copyright = new Date();
+let oLastModif = new Date(document.lastModified);
 
-currentyear.innerHTML = `©️ <span class="highlight">${today.getFullYear()}</span>`;
+currentyear.innerHTML = `current year is: <span class="highlight">${today.getFullYear()}</span>`;
+copyrightyear.innerHTML = `©️ <span class="highlight">${copyright.getFullYear()}</span>`;
+last_modified.innerHTML = `last modified <span class="highlight">${oLastModif}</span>`;
